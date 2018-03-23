90263a2034e1a90338551e2602634591c4d461f9
morgannegagne <morgannegagne@gmail.com>
2018-03-23 14:28:51 -0400

    added insurance route

--- app/controllers/api/v1/doctor_database_controller.rb
+++ app/controllers/api/v1/doctor_database_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::DoctorDatabaseController < ApplicationController
+  before_action :set_api_key
 
-  def index
-    api_key = ENV["better_doctors_api_key"]
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-    @doctors = RestClient.get("https://api.betterdoctor.com/2016-03-01/doctors?location=#{longitude}%2C#{latitude}%2C5&user_location=#{longitude}%2C#{latitude}&skip=0&limit=10&user_key=#{api_key}")
+  def doctors
+    @doctors = RestClient.get("https://api.betterdoctor.com/2016-03-01/doctors?location=#{params[:latitude]}%2C#{params[:longitude]}%2C5&user_location=#{longitude}%2C#{latitude}&skip=0&limit=10&user_key=#{@api_key}")
     render json: @doctors
+  end
+
+  def insurances
+    @insurances = RestClient.get("https://api.betterdoctor.com/2016-03-01/insurances?user_key=#{@api_key}")
+    render json: @insurances
+  end
+
+  private
+
+  def set_api_key
+    @api_key = ENV["better_doctors_api_key"]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :update, :create, :destroy]
-      resources :doctor_database, only: [:index, :show]
       resources :google, only: [:create]
     end
   end
+
+  get 'api/v1/doctor_database/doctors', to: 'api/v1/doctor_database#doctors'
+  get 'api/v1/doctor_database/insurances', to: 'api/v1/doctor_database#insurances'
 
 end
