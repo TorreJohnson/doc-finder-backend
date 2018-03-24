f7e19fa73c99fcdf8b6f23ef9441506eab3bba62
morgannegagne <morgannegagne@gmail.com>
2018-03-24 16:38:26 -0400

    added query params to api request

--- app/controllers/api/v1/doctor_database_controller.rb
+++ app/controllers/api/v1/doctor_database_controller.rb
@@ -1,20 +1,25 @@
 class Api::V1::DoctorDatabaseController < ApplicationController
-  before_action :set_api_key
+  before_action :set_api_params
 
   def doctors
-    @doctors = RestClient.get("https://api.betterdoctor.com/2016-03-01/doctors?location=#{params[:latitude]}%2C#{params[:longitude]}%2C5&user_location=#{longitude}%2C#{latitude}&skip=0&limit=10&user_key=#{@api_key}")
+    if params[:q] != ''
+      @doctors = RestClient.get("#{@base_url}/doctors?query=#{params[:q]}&location=#{params[:latitude]}%2C#{params[:longitude]}%2C10&user_location=#{params[:latitude]}%2C#{params[:longitude]}&skip=0&limit=10&user_key=#{@api_key}")
+    else
+      @doctors = RestClient.get("#{@base_url}/doctors?location=#{params[:latitude]}%2C#{params[:longitude]}%2C10&user_location=#{params[:latitude]}%2C#{params[:longitude]}&skip=0&limit=10&user_key=#{@api_key}")
+    end
     render json: @doctors
   end
 
   def insurances
-    @insurances = RestClient.get("https://api.betterdoctor.com/2016-03-01/insurances?user_key=#{@api_key}")
+    @insurances = RestClient.get("#{@base_url}/insurances?user_key=#{@api_key}")
     render json: @insurances
   end
 
   private
 
-  def set_api_key
+  def set_api_params
     @api_key = ENV["better_doctors_api_key"]
+    @base_url = "https://api.betterdoctor.com/2016-03-01"
   end
 
 end
