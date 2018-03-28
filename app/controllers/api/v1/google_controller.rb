41d7ae0616acd382e2e8e08f3834c4ea32ffcf42
morgannegagne <morgannegagne@gmail.com>
2018-03-28 11:39:19 -0400

    final commit before deploying to heroku

--- app/controllers/api/v1/google_controller.rb
+++ app/controllers/api/v1/google_controller.rb
@@ -6,8 +6,13 @@ class Api::V1::GoogleController < ApplicationController
     location = params["location"].split(' ').join('+')
     @result = RestClient.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{api_key}")
     @json = JSON.parse(@result)
-    @coordinates = @json["results"][0]["geometry"]["location"]
-    render json: @coordinates
+    if @json["status"] == "OK"
+      @coordinates = @json["results"][0]["geometry"]["location"]
+      render json: @coordinates
+    else
+      render json: {"error": "location not found"}
+    end
+
   end
 
 end
